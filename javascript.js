020b6453ea6409fead4f16a908deead31f7d135f
Ferdousnayeem <mostafanayeem11@gmail.com>
2022-01-28 08:20:53 +0600

    bootstrap

--- javascript.js
+++ javascript.js
@@ -1,2 +1,17 @@
-console.log(570);
-console.log(56);
+var unitPrice = 5;
+var quantity = 10;
+
+var totalPrice = unitPrice + quantity; 
+
+// console.log(totalPrice);
+// console.log(unitPrice*quantity);
+
+// var age = 21;
+// age += 8;
+// console.log(age);
+// console.log(17%5);
+
+var num1= "99.5";
+var num2= 15;
+// console.log(num1+num2);
+console.log(6!=6);
